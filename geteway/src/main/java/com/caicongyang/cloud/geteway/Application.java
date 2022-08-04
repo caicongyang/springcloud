63a8ac4a534e4d3ea29275e91fa7c441d0f013d4
caicongyang <caicongyang@gmail.com>
2022-08-04 16:56:00 +0800

    rename packename

--- geteway/src/main/java/com/caicongyang/cloud/geteway/Application.java
+++ geteway/src/main/java/com/caicongyang/cloud/geteway/Application.java
@@ -1,4 +1,4 @@
-package com.caicongyang.cloud.getway;
+package com.caicongyang.cloud.geteway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
